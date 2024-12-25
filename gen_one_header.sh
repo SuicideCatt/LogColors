c8908929ff9f1df801e3aaef4e23a03a5474a291
SuicideCatt <random.cat.from.internet@gmail.com>
2024-12-25 18:28:13 +0200

    ci: try touch gen_one_header.sh result file

--- gen_one_header.sh
+++ gen_one_header.sh
@@ -6,6 +6,7 @@ cd build
 VERSION=$(cmake --system-information | awk -F= '$1~/PROJECT_VERSION:STATIC/{print$2}')
 cd ..
 
+touch "$3"
 printf "// $1 $VERSION\n// $2\n\n" > "$3" && cat "$3"
 
 ~/.local/bin/quom "./include/$1.hpp" "/tmp/$1.hpp" && cat "/tmp/$1.hpp" >> "$3"
